bc861be4b26a6c743942877653b15b73b90e5d3e
Stuart Garvagh <sgarvagh@uoguelph.ca>
2013-07-10 11:39:36 -0400

    1st commit, added bootstrap

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,16 @@
+class SessionController < ApplicationController
+
+	def create
+		@user = User.find_by_email params[:email]
+		if @user && @user.authenticate params[:password] # login
+			session[:user_id] = @user.id
+			redirect_to root_url
+		else #wrong login details
+			flash.now[:alert] = "Wrong login details"
+		end
+	end
+
+	def destroy
+		session[:user_id] = nil
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rainforest2::Application.config.secret_key_base = 'a438b0a9bd012367d8a07ad3fbc2e08d9994f6feef15d9e1324353d8884128a999ef3ec37421fbd2a76fe8cef04eaea46f7f2c47385fabad7ee8835e66e9624c'
